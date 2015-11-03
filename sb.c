995c7857eeb0ba90300ecd17eacc4a23fd24959f
Demur Rumed <gunkmute@gmail.com>
2015-11-03 03:17:47 +0000

    -glOrtho

--- sb.c
+++ sb.c
@@ -6,11 +6,10 @@ int main(int argc,char**argv){
 	Window Wdo=XCreateWindow(dpy,RootWindow(dpy,vi->screen),0,0,1680,1050,0,vi->depth,InputOutput,vi->visual,CWColormap|CWEventMask,(XSetWindowAttributes[]){{.colormap=XCreateColormap(dpy,RootWindow(dpy,vi->screen),vi->visual,AllocNone)}});
 	XMapWindow(dpy,Wdo);
 	glXMakeCurrent(dpy,Wdo,glXCreateContext(dpy,vi,0,GL_TRUE));
-	glOrtho(0,1680,1050,0,1,-1);
 	static const unsigned char col[][3]={{255,255,255},{0,0,0}};
 	for(;;){
 		glColor3ubv(col[rand()&1]);
-		glRecti(0,0,1680,1050);
+		glRecti(-1,-1,1,1);
 		glFinish();
 	}
 }
